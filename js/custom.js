c291424ed24999024441bb9cf9ce618a2b4ded71
tszhong0411 <lainelson411@gmail.com>
2021-03-06 05:11:20 +0800

    網站更新於 2021-03-06 05:11:07

--- js/custom.js
+++ js/custom.js
@@ -907,4 +907,93 @@ var calLanguages = {
     ],
     dayOfWeek: ['კვირა', 'ორშაბათი', 'სამშაბათი', 'ოთხშაბათი', 'ხუთშაბათი', 'პარასკევი', 'შაბათი']
   },
-};</a></td></tr></tbody></table></a></div></div></div></div>
+};
+
+// Clock JS
+var mykey = {
+  weather: 'efc89c3ffc8066e43ec72767c8d15d79' //改為你從OpenWeather獲取的API密鑰
+};
+var locationurl = 'https://extreme-ip-lookup.com/json/';
+var cityname = '';
+var weatherurl = '';
+var userip = '';
+var week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
+fetch(locationurl)
+  .then(data =&gt; data.json())
+  .then(data =&gt; {
+    //console.log(data);
+    cityname = data.city;
+    if (typeof data.city == "undefined") {
+      cityname = data.region;
+    };
+    if (typeof data.region == "undefined") {
+      cityname = data.country;
+    };
+    userip = data.query;
+    weatherurl = 'https://api.openweathermap.org/data/2.5/weather/?q=' + cityname + '&amp;units=metric&amp;appid=' + mykey.weather;
+    getweatherdata();
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+function getweatherdata() {
+  fetch(weatherurl)
+    .then(data =&gt; data.json())
+    .then(data =&gt; {
+      //console.log(data);
+      clock.weatherimg = 'https://cdn.jsdelivr.net/gh/tszhong0411/image' + data.weather[0].icon + '.png';
+      clock.temperature = data.main.temp + "*C";
+      clock.humidity = data.main.humidity + "%";
+      clock.ip = userip;
+      clock.humidityimg = 'https://cdn.jsdelivr.net/gh/tszhong0411/image/hu.png';
+      clock.city = data.name;
+      var timerID = setInterval(updateTime, 1000);
+      updateTime();
+      clock.clockshow = true;
+
+      function updateTime() {
+        var cd = new Date();
+        clock.time = zeroPadding(cd.getHours(), 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
+        clock.date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth() + 1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
+        var hamorpm = cd.getHours();
+        var str;
+        if (hamorpm &gt; 12) {
+          hamorpm -= 12;
+          str = " PM";
+        } else {
+          str = " AM";
+        }
+        clock.daylight = str
+      };
+
+      function zeroPadding(num, digit) {
+        var zero = '';
+        for (var i = 0; i &lt; digit; i++) {
+          zero += '0';
+        }
+        return (zero + num).slice(-digit);
+      };
+      updateTime();
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+};
+var clock = new Vue({
+  el: '#clock',
+  data: {
+    ip: '',
+    time: '',
+    weatherimg: '',
+    temperature: '',
+    humidityimg: '',
+    humidity: '',
+    usaqi: '',
+    city: '',
+    date: '',
+    daylight: '',
+    clockshow: 'false'
+  },
+});</a></td></tr></tbody></table></a></div></div></div></div>
